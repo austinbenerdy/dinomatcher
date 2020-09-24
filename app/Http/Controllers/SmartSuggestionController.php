d44f034d17101c2149a5129fb47a443cee0ac420
Austin Adamson <austin.l.adamson@gmail.com>
2020-09-24 08:43:20 -0700

    Correcting controller logic for match post command

--- app/Http/Controllers/SmartSuggestionController.php
+++ app/Http/Controllers/SmartSuggestionController.php
@@ -30,19 +30,7 @@ class SmartSuggestionController extends Controller
         $name = $request->get('name');
         $color = $request->get('color');
 
-        $nameLength = strlen($name);
-
-        if ($color === 'green') {
-            $dino = Dinosaur::find(1);
-        } else if ($nameLength > 30) {
-            $dino = Dinosaur::find(2);
-        } else if ($nameLength > 15 && $nameLength < 20 && in_array(['brown', 'yellow', 'orange'], $color)) {
-            $dino = Dinosaur::find(3);
-        } else if ($nameLength > 10 && $nameLength < 20 && in_array(['red', 'blue', 'pink'], $color)) {
-            $dino = Dinosaur::find(5);
-        } else {
-            $dino = Dinosaur::find(4);
-        }
+        $dino = $this->getMatch($name, $color);
 
         return response($dino, 200);
     }
@@ -56,6 +44,13 @@ class SmartSuggestionController extends Controller
         $name = $request->get('name');
         $color = $request->get('color');
 
+        $dino = $this->getMatch($name, $color);
+
+        return response($dino, 200);
+    }
+
+    private  function getMatch(string $name, string $color): Dinosaur
+    {
         $nameLength = strlen($name);
 
         if ($color === 'green') {
@@ -70,6 +65,6 @@ class SmartSuggestionController extends Controller
             $dino = Dinosaur::find(4);
         }
 
-        return response($dino, 200);
+        return $dino;
     }
 }
